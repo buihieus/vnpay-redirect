985e5b676586bbd2266d611d78c67b2c4a2d00a2
Bui Cong Hieu <buihieu9259@gmail.com>
2025-06-16 17:22:13 +0700

    test

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const params = new URLSearchParams(window.location.search);
-      const query = params.toString();
-      const redirectUrl = `ecommerce_app://payment/return?${query}`;
+const query = params.toString();
+const redirectUrl = `ecommerce_app://payment/return?${query}`;
 
-      setTimeout(() => {
-        window.location.href = redirectUrl;
-      }, 1500);
+// Redirect đến app sau 1.5 giây
+setTimeout(() => {
+  window.location.href = redirectUrl;
+
+  // Fallback: nếu không mở được app sau 3 giây thì về trang chủ
+  setTimeout(() => {
+    window.location.href = "/";
+  }, 3000);
+}, 1500);
