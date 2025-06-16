1764ddcc312ffa7253cb4d67172b01849ee33b6f
Bui Cong Hieu <buihieu9259@gmail.com>
2025-06-16 16:17:44 +0700

    update

---
+++ app.js
@@ -0,0 +1,7 @@
+const params = new URLSearchParams(window.location.search);
+      const query = params.toString();
+      // const redirectUrl = `ecommerce_app://payment/return?${query}`;
+
+      setTimeout(() => {
+        window.location.href = redirectUrl;
+      }, 1500);
