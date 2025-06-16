3acab36ff360b94908cb232fcd4c1d31ef081f79
Bui Cong Hieu <buihieu9259@gmail.com>
2025-06-16 16:51:41 +0700

    fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const params = new URLSearchParams(window.location.search);
       const query = params.toString();
-      // const redirectUrl = `ecommerce_app://payment/return?${query}`;
+      const redirectUrl = `ecommerce_app://payment/return?${query}`;
 
       setTimeout(() => {
         window.location.href = redirectUrl;
